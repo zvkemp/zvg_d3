011830e9f8eefa9b888190df664988673eb0c543
zvkemp <zvkemp@gmail.com>
2014-10-27 12:30:58 -0700

    sinatra setup

--- app.rb
+++ app.rb
@@ -3,3 +3,21 @@ require 'json'
 require 'haml'
 require 'coffee-script'
 
+
+class ZVGDemo < Sinatra::Base
+
+  get '/' do
+    haml :index
+  end
+
+  get '/d3.js' do
+    content_type "text/javascript"
+    File.read(File.expand_path("./vendor/d3.v3.min.js"))
+  end
+
+  get '/zvg/*.js' do
+    (coffee File.read(File.expand_path("./app/assets/javascripts/zvg/#{params[:splat][0]}.coffee")))
+  end
+
+  run! if app_file == $0
+end
--- lib/zvg_d3.rb
+++ lib/zvg_d3.rb
@@ -1,8 +1,9 @@
 require "zvg_d3/version"
-
-module ZvgD3
-  module Rails
-    class Engine < ::Rails::Engine
+if defined?(Rails)
+  module ZvgD3
+    module Rails
+      class Engine < ::Rails::Engine
+      end
     end
   end
 end
