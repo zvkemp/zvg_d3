dd5e5adbda5905a4cddd0e4531102a3eccb51342
zvkemp <zvkemp@gmail.com>
2014-10-28 10:14:32 -0700

    etc...

--- app.rb
+++ app.rb
@@ -10,6 +10,11 @@ class ZVGDemo < Sinatra::Base
     haml :index
   end
 
+  get '/zvg.css' do
+    content_type "text/css"
+    File.read(File.expand_path("./app/assets/stylesheets/zvg.css"))
+  end
+
   get '/d3.js' do
     content_type "text/javascript"
     File.read(File.expand_path("./vendor/d3.v3.min.js"))
